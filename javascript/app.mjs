56ec5249412c31e8ca5f1a9dc3a11b1fa67ede03
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-08-08 18:06:04 +0200

    Manage case that not need sum of two number

--- javascript/app.mjs
+++ javascript/app.mjs
@@ -7,11 +7,15 @@ export default class App {
 	 * @returns Array of indexes or empty Array
 	 */
 	static twoSum(numberArray, target) {
+		if (numberArray.includes(target)) return [numberArray.indexOf(target)];
 		let result = [];
-		for (const number of numberArray) {
-			const complement = target - number;
+		for (const currentNumber of numberArray) {
+			const complement = target - currentNumber;
 			if (complement > 0 && numberArray.includes(complement)) {
-				result = [numberArray.indexOf(complement), numberArray.indexOf(number)];
+				result = [
+					numberArray.indexOf(currentNumber),
+					numberArray.indexOf(complement),
+				];
 				break;
 			}
 		}
@@ -19,16 +23,24 @@ export default class App {
 	}
 }
 
+const expected_cases = [2, 4, 8, 16, 32];
+
 [
 	{ target: 12, expect_indexes: [1, 2] },
 	{ target: 34, expect_indexes: [0, 4] },
 	{ target: 18, expect_indexes: [0, 3] },
+	{ target: 16, expect_indexes: [expected_cases.indexOf(16)] },
+	{ target: 32, expect_indexes: [expected_cases.indexOf(32)] },
 	{ target: 42, expect_indexes: [] },
 ].forEach((expect) =>
 	console.assert(
-		JSON.stringify(App.twoSum([2, 4, 8, 16, 32], expect.target)) ===
+		JSON.stringify(App.twoSum(expected_cases, expect.target)) ===
 			JSON.stringify(expect.expect_indexes),
-		new Error(`Sum for total of "${expect.target}"`)
+		new Error(
+			`Sum for total of "${expect.target}" not ${JSON.stringify(
+				App.twoSum(expected_cases, expect.target)
+			)}`
+		)
 	)
 );
 console.log("Everything is OK");
