9fd24efac356a7a01c138982338bf87d90cd6768
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-08-09 13:43:59 +0200

    WIP: findAllIndexes

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,13 +1,33 @@
 export default class App {
 	/**
+	 * Sort desc and filter numbers less than {maxNumber} in {numberArray}
+	 * @param numberArray Array to filter
+	 * @param maxNumber maximum number not to be exceeded
+	 * @returns new array of numbers
+	 */
+	private static sortAndFilterLessThan(
+		numberArray: Array<number>,
+		maxNumber: number
+	): Array<number> {
+		return numberArray.sort((a, b) => b - a).filter((n) => n > maxNumber);
+	}
+
+	private static sumOfEquals(
+		numberArray: Array<number>,
+		target: number
+	): boolean {
+		return numberArray.reduce((acc, value) => (acc += value), 0) == target;
+	}
+
+	/**
 	 * Find 2 indexes in the array that the addition gives the target number
 	 * @param {Array} numberArray Array of numbers
 	 * @param {number} target result expected
 	 * @returns Array of indexes or empty Array
 	 */
 	static twoSum(numberArray: Array<number>, target: number): Array<number> {
-		let result: Array<number> = [];
 		if (numberArray.includes(target)) return [numberArray.indexOf(target)];
+		let result: Array<number> = [];
 		for (const number of numberArray) {
 			const complement: number = target - number;
 			if (complement > 0 && numberArray.includes(complement)) {
@@ -17,9 +37,39 @@ export default class App {
 		}
 		return result;
 	}
+
+	static findAllIndexes(
+		numberArray: Array<number>,
+		target: number
+	): Array<number> {
+		let numArray = App.sortAndFilterLessThan(numberArray, target);
+		let keepLoop = true;
+		let i = 0;
+		let numbers: Array<number> = [];
+		while (keepLoop) {
+			if (!numArray.length) keepLoop = false;
+
+			if (App.sumOfEquals([...numArray], target)) {
+				return numArray;
+			}
+
+			const complement: number = target - numArray[0];
+			numbers.push(numArray[0]);
+			numArray = App.sortAndFilterLessThan(numberArray, complement);
+			if (!numArray.length) {
+				numArray.shift();
+				numbers.pop();
+				continue;
+			}
+
+			if (!numArray.includes(complement)) {
+				continue;
+			}
+		}
+	}
 }
 
-const numberArray: Array<number> = [2, 4, 8, 16, 32];
+const numberArray: Array<number> = [1, 2, 4, 8]; // 14 2 => 2 -> 12 -> not in [2, 4, 8, 16, 32] then [4, 8,] 12 - 4 = 8 in [ 4, 8] True then [indexOf(2), indexOf(4), indexOf(8)]
 
 interface ExpectedCase {
 	target: number;
